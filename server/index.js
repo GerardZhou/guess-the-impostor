b24d8c387c99a5da216ac3019423d73fda426e11
GerardZhou <dorey1200@gmail.com>
2025-03-02 15:59:03 -0600

    figured out connecting backend to frontend

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hi from backend");
 });
+
 app.listen(8080, () => {
   console.log("server running");
 });
