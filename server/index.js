5dbeda58f900468ae5b4b99670a0a1ecf73e2ae8
GerardZhou <dorey1200@gmail.com>
2025-03-02 15:32:47 -0600

    imported axios and figured out backend

--- server/index.js
+++ server/index.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const app = express();
 
-console.log(app);
+app.get("/", (req, res) => {
+  res.send("Hi from backend");
+});
+app.listen(8080, () => {
+  console.log("server running");
+});
